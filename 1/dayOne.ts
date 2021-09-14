e138aba334e6612b5368cd21165acc70cf54bb72
Darja Port <darja.port@gmail.com>
2021-09-14 22:42:26 +0200

    Day-one end

--- 1/dayOne.ts
+++ 1/dayOne.ts
@@ -1,19 +1,22 @@
-const fs = require('fs')
+(function(){
+    const fs = require('fs')
+    let file = fs.readFileSync("dayTwo.txt").toString('utf-8')
+    let nums:string[] = file.split("\n");
 
-let file = fs.readFileSync("dayOne.txt").toString('utf-8')
-let nums:string[] = file.split("\n");
-
-let num:number = 2020;
-
-function findNum(arr:string[]) {
-    for(let i = 0; i<nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (parseInt(nums[i]) + parseInt(nums[j]) === num) {
-                return (parseInt(nums[i]) * parseInt(nums[j]))
+    let num:number = 2020;
+    
+    function findNum(arr:string[]) {
+        for(let i = 0; i<nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                for (let k = j+1; k < nums.length; k++) {
+                    if (parseInt(nums[i]) + parseInt(nums[j]) + parseInt(nums[k]) === num) {
+                        return (parseInt(nums[i]) * parseInt(nums[j]) * parseInt(nums[k]))
+                    }
+                }
             }
         }
-        parseInt(nums[i])
     }
-}
-console.log(findNum(nums))
-// Answer: 956091
+    console.log(findNum(nums))
+    // Answer: 956091
+    // Answer: 79734368
+}());
