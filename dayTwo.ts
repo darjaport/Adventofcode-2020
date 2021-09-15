4235878b280181c5d83187e16ba72032ddc42d99
Darja Port <darja.port@gmail.com>
2021-09-15 08:20:44 +0200

    Day-two-end

--- dayTwo.ts
+++ dayTwo.ts
@@ -3,29 +3,29 @@
     let file = fs.readFileSync("dayTwo.txt").toString('utf-8')
     let line:string[] = file.split("\n")
 
+    let arr:string[]
     let total:number = 0
     let totalChar:number = 0
-    let minMaxNum:any
+    let minMaxNum:string[]
     let password:string
     let letter:string
 
     function passwordValidation() {
         for (let i = 0; i < line.length; i++) {
+
+            arr = line[i].split(" ")
             minMaxNum = line[i].match(/\d+/g)
-            password = line[i].split(" ")[2]
-            letter = line[i].split(" ")[1][0]
+            password = arr[2]
+            letter = arr[1][0]
+    
+            if (letter === password[parseInt(minMaxNum[0])-1] && letter !== password[parseInt(minMaxNum[1])-1]) total++
+            if (letter !== password[parseInt(minMaxNum[0])-1] && letter === password[parseInt(minMaxNum[1])-1]) total++
 
-            for (let j = 0; j < password.length; j++) {
-                if (password[j] === letter) totalChar++
-            }
-        
-            if (totalChar >= parseInt(minMaxNum[0]) && totalChar <= parseInt(minMaxNum[1])) total++
-
-            totalChar = 0
         }
         return total
     }
 
     console.log(passwordValidation())
+    // Answer: 500
 
 })();
