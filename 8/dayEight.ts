0d2f72e301e6eb184f41ffd1b22de017a78b446a
Darja Port <darja.port@gmail.com>
2021-10-21 19:43:25 +0200

    Day-eight-fix

--- 8/dayEight.ts
+++ 8/dayEight.ts
@@ -1,47 +1,35 @@
-// instruction -> 
-    // acc (increases/decreases), 
-    // jmp (jumps to a new instruction), jmp +2 skip one, jmp +1 next
-    // nop (No OPeration) it does nothing
-// argument -> +4 / -20
-// accumulator -> global value, starts at 0
-
 (function(){
+
     const fs = require('fs')
     let file = fs.readFileSync("dayEight.txt").toString('utf-8').split('\n')
 
     let acc:number = 0
     let i:number = 0
+    interface Instructions {
+        [key: number]: number
+    }
+    let instructions:Instructions = {}
     
     while (i < file.length) {
-        let char:string = file[i].match("[+,-]")[0]
-        let num:number = parseInt(file[i].match(/\d+/g)[0])
+        let num:number = parseInt(file[i].match(/-?\d+/)[0])
+        let char:String = file[i].substring(0, 3)
+
+        if(instructions[i]) break
+        else instructions[i] = i
         
-        switch(file[i].substring(0, 3)) {
+        switch(char) {
             case "acc":
-                console.log(acc)
-                if (file[i].charAt(file[i].length - 1) === "*") return
-                file[i] = file[i].concat("*")
-                if (char === "+") acc += num
-                else if (char === "-") acc -= num
+                acc += num
                 i++
-                
                 break
             case "jmp":
-                console.log(acc)
-                if (file[i].charAt(file[i].length - 1) === "*") return
-                file[i] = file[i].concat("*")
-                if (char === "+") i += num
-                else if (char === "-") i -= num
+                i += num
                 break
             case "nop":
-                console.log(acc)
-                if (file[i].charAt(file[i].length - 1) === "*") return
-                file[i] = file[i].concat("*")
                 i++
                 break
-            default:
-                return
         }
     }
-    console.log(acc);
+    console.log("First star: " + acc);
+    
 })()
