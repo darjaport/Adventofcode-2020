507d0caefa003d9775a509090b0ea332cc5f7c10
Darja Port <darja.port@gmail.com>
2021-10-21 15:51:34 +0200

    Day-thirteen-fix

--- 13/dayThirteen.ts
+++ 13/dayThirteen.ts
@@ -5,19 +5,24 @@
    
     let departure:number = parseInt(file[0][0])
     file = file[1].filter((item:string) => item !== "x")
-    let p:number[] = []
+    
+    interface Values {
+        [key: number]: number
+    }
+    
+    let p:Values = {}
 
     file.map((item:string) => {
         let num:number = parseInt(item)
         
         for (let i:number = num; i <= departure+num; i+=num) {
-            if (i >= departure) p.push(i)
+            if (i >= departure) p[num] = i
         }
     })
 
-    let id:number = parseInt(file[p.indexOf(Math.min(...p))])
-    let min:number = p[p.indexOf(Math.min(...p))] - departure
-
-    console.log(id*min);
+    let minsToWait = Math.min(...Object.values(p))
+    let id:any = Object.keys(p).find((key:any) => p[key] === minsToWait)
+    
+    console.log("First star: " + ((minsToWait - departure) * id));
     
 })()
